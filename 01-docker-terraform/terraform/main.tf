7542dd935216355183bd7ffba423fbf6c4dd21b6
mdzki <mikill.priv@gmail.com>
2026-01-23 08:33:04 +0100

    Add gcs-bucket via tf

--- 01-docker-terraform/terraform/main.tf
+++ 01-docker-terraform/terraform/main.tf
@@ -1,14 +1,28 @@
 terraform {
   required_providers {
     google = {
-      source = "hashicorp/google"
+      source  = "hashicorp/google"
       version = "7.16.0"
     }
   }
 }
 
 provider "google" {
-    project     = "terraform-demo-485018"
-    region      = "europe-central2"
+  project = "terraform-demo-485018"
+  region  = "europe-central2"
 }
 
+resource "google_storage_bucket" "auto-expire" {
+  name          = "auto-expiring-bucket"
+  location      = "eu"
+  force_destroy = true
+
+  lifecycle_rule {
+    condition {
+      age = 1
+    }
+    action {
+      type = "AbortIncompleteMultipartUpload"
+    }
+  }
+}
